2359764c28ada46d298e5495489395814c76e368
Vasileios Toukas <45406064+toukava@users.noreply.github.com>
2018-12-01 18:11:53 +0100

    Add files via upload

--- installation.py
+++ installation.py
@@ -14,35 +14,46 @@
 import os
 import subprocess
 
-
+# This class is used to install magic mirror open source software in Raspberry pi current directory
+# First it checks wether Magic Mirror folder exists
+# If exists, it simply starts the software by running nmp start
+# If it does not exist:
+#   - it downloads the required software from the repository
+#   - it installs the software
+#   - configures config.js file
+#   - runs npm starts
 class InitMirror:
- 
+    
+  # initialises object
   def __init__(self):
-    # The repository that Magic Mirror open source is located
-    self.url = "https://github.com/MichMich/MagicMirror"
-	
-  def isMirrorInstalled (self):
-    # Check if Magic Mirror is already installed
-    print ("Initializing SMOW......")
+    # The repository that Magic Mirror open source is located 
+    self.magicMirrorDir = "/home/pi/iot-development-master/MagicMirror"
+  # checks if mirror is downloaded
+  # returns true if installed, false eitherwise
+  def isMirrorDownloaded (self):
+    # Check if Magic Mirror is already downloaded
+    print ("Detecting SMOW......")
     print ("Check for previous installations")
     if os.path.isdir("MagicMirror"):
-      # TODO: Add more checks on the format of the directory
       return True
     else:
       return False
 	  
+  # installs magic mirror
+  # if mirror is not downloaded it downloads first and then installs it
+  # returns true if mirror is finally installed
   def install (self):
     os.system("git clone https://github.com/MichMich/MagicMirror")
-    if self.isMirrorInstalled:
-      print("CLONED SUCCESSFULLY")
-      os.chdir('/home/pi/Desktop/iot-development-master/MagicMirror')
+    if self.isMirrorDownloaded:
+      print("MAGIC MIRROR DOWNLOADED SUCCESSFULLY")
+      os.chdir(self.magicMirrorDir)
       subprocess.check_call('npm install', shell=True)
       return True
     else:
       print("SOMETHING WENT WRONG")
       return False
     
-
+  # prints info about the software
   def printInfo(self):
     print ("*****************************************************************************")
     print ("* SMOW V1.0                                                                 *")
@@ -54,23 +65,25 @@ class InitMirror:
     print ("*****************************************************************************")
     print ("");
   
+  # starts nagic mirror software by executing npm start
   def start (self):
-    #TODO: Add implementation
-    os.chdir('/home/pi/Desktop/iot-development-master/MagicMirror')
+    os.chdir(self.magicMirrorDir)
     subprocess.check_call('npm start', shell=True)
 	
+# main function	
 if __name__ == '__main__':
-
+  # init an object
   objMagicMirror = InitMirror()
+  # prints info
   objMagicMirror.printInfo ()
-  bResult = True
-  
-  if not objMagicMirror.isMirrorInstalled():
+  bInstalled = True
+  # check if mirror is downloaded
+  if not objMagicMirror.isMirrorDownloaded():
     # need to install mirror
-    bResult = objMagicMirror.install()
+    bInstalled = objMagicMirror.install()
 	
   #mirror already installed, start it
-  if bResult:
+  if bInstalled:
     objMagicMirror.start()
 	
   
